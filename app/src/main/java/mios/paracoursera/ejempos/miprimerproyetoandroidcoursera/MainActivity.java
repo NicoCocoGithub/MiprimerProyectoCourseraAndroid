e363a3bcc98275103f515bafd935393e9a5a6914
nicolas <cocodibuja@gmail.com>
2016-04-17 22:09:49 -0300

    Initial commit

---
+++ app/src/main/java/mios/paracoursera/ejempos/miprimerproyetoandroidcoursera/MainActivity.java
@@ -0,0 +1,16 @@
+package mios.paracoursera.ejempos.miprimerproyetoandroidcoursera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // Primer Commit 1 ...
+    }
+}
